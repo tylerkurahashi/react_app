8a4bf2b147aa06978b61f5019c737c564b297538
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-04-30 17:10:49 +0900

    fixed div

--- src/components/js/ExpenseItem.js
+++ src/components/js/ExpenseItem.js
@@ -6,14 +6,13 @@ function ExpenseItem(props) {
     return (
         <Card className="expense-item">
             <ExpenseDate
-            date={props.date}
+                date={props.date}
             />
-            <div>
-                <div className="expense-item__description">
-                    <h2>{props.title}</h2>
-                    <div className="expense-item__price">${props.amount}</div>
-                </div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button>Change Title</button>
         </Card>
     )
 }
