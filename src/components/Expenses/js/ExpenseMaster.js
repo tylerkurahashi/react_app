b2faaed20a8f305c29e8f2bb8bac8d8d30258045
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-05-02 23:06:15 +0900

    filter implemented

--- src/components/Expenses/js/ExpenseMaster.js
+++ src/components/Expenses/js/ExpenseMaster.js
@@ -1,22 +1,36 @@
+import {useState} from 'react';
+
 import '../css/ExpenseMaster.css';
 import Card from "../../UI/js/Card";
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 function ExpenseMaster(props) {
     let items = props.expenses.map((expense, index) => {
         return (<ExpenseItem
-        key={index}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
+            key={index}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
         />);
     });
 
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
 
     return (
-        <Card className='expenses'>
-            {items}
-        </Card>
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
+                />
+                {items}
+            </Card>
+        </div>
     )
 }
 
